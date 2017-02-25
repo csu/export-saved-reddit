3bec6e65188b5af0976224b8ffc0d4df0032c0ff
rachmadaniHaryono <foreturiga@gmail.com>
2017-02-25 09:42:41 +0800

    new: test: test for PR.

--- tests/test_export_saved.py
+++ tests/test_export_saved.py
@@ -1,4 +1,5 @@
 """test module."""
+from itertools import product
 from unittest import mock
 import argparse
 
@@ -23,6 +24,11 @@ def test_get_args(argv, exp_res):
     assert res.__dict__ == exp_res
 
 
+def get_str_permalink():
+    """Return string permalink."""
+    return 'permalink'
+
+
 @pytest.mark.parametrize(
     'item, csv_rows',
     [
@@ -35,6 +41,11 @@ def test_get_args(argv, exp_res):
         (
             argparse.Namespace(
                 title='title', permalink='permalink', created='invalid'),
+            ['https://www.reddit.com/permalink', 'title', 0, None, 'None']
+        ),
+        (
+            argparse.Namespace(
+                title='title', permalink=get_str_permalink, created='invalid'),
             ['https://www.reddit.com/permalink', 'title', 0, None, 'None']
         ),
     ]
@@ -58,3 +69,113 @@ def test_write_csv():
             mock.call.write('URL,Title,Created,Selection,Folder\r\n'),
             mock.call.write('url1,title1,10,,folder1\r\n')
         ])
+
+
+def test_login():
+    """test func."""
+    args = mock.Mock()
+    account = {
+        'username': mock.Mock(),
+        'password': mock.Mock(),
+        'client_id': mock.Mock(),
+        'client_secret': mock.Mock(),
+    }
+    with mock.patch('export_saved.praw') as m_praw, \
+            mock.patch('export_saved.account_details') as m_ad:
+        m_ad.return_value = account
+        from export_saved import login
+        res = login(args)
+        m_ad.assert_called_once_with(args=args)
+        assert res == m_praw.Reddit.return_value
+
+
+@pytest.mark.parametrize(
+    'username, password, client_id, client_secret',
+    product([None, mock.Mock()], repeat=4)
+)
+def test_account_details(username, password, client_id, client_secret):
+    """test account details."""
+    ns_kwargs = {
+        'username': username,
+        'password': password,
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
+    args = argparse.Namespace(**ns_kwargs)
+    cond_match = all(ns_kwargs[key] for key in ns_kwargs)
+    from export_saved import account_details
+    if not cond_match:
+        with pytest.raises(SystemExit):
+            account_details(args)
+        return
+    res = account_details(args)
+    assert res == ns_kwargs
+
+
+def test_process():
+    """test func."""
+    reddit = mock.Mock()
+    seq = mock.Mock()
+    file_name = 'filename'
+    folder_name = 'folder_name'
+    with mock.patch('export_saved.get_csv_rows'),\
+            mock.patch('export_saved.write_csv'),\
+            mock.patch('export_saved.Converter') as m_converter:
+        from export_saved import process
+        # run
+        process(reddit, seq, file_name, folder_name)
+        # test
+        m_converter.assert_has_calls([
+            mock.call('filename.csv', 'filename.html', folder_name=folder_name),
+            mock.call().convert()
+        ])
+
+
+@pytest.mark.parametrize('key', ['upvoted', 'saved', 'comments', 'submissions'])
+def test_save(key):
+    """test func."""
+    reddit = mock.Mock()
+    seq = mock.Mock()
+    if key in ('saved', 'upvoted'):
+        getattr(reddit.user.me.return_value, key).return_value = seq
+    else:
+        getattr(reddit.user.me.return_value, key).new.return_value = seq
+    with mock.patch('export_saved.process') as m_p:
+        import export_saved
+        getattr(export_saved, 'save_{}'.format(key))(reddit)
+        m_p.assert_called_once_with(
+            reddit, seq, 'export-{}'.format(key), "Reddit - {}".format(key.title()))
+
+
+@pytest.mark.parametrize('verbose, upvoted, all', product([True, False], repeat=3))
+def test_main(verbose, upvoted, all):
+    """test func"""
+    reddit = mock.Mock()
+    with mock.patch('export_saved.get_args') as m_ga, \
+            mock.patch('export_saved.logging') as m_logging, \
+            mock.patch('export_saved.login') as m_login, \
+            mock.patch('export_saved.save_upvoted'), \
+            mock.patch('export_saved.save_saved'), \
+            mock.patch('export_saved.save_submissions'), \
+            mock.patch('export_saved.save_comments'):
+        m_login.return_value = reddit
+        m_ga.return_value = argparse.Namespace(
+            verbose=verbose,
+            upvoted=upvoted,
+            all=all,
+        )
+        import export_saved
+        # run
+        with pytest.raises(SystemExit):
+            export_saved.main()
+        if verbose:
+            m_logging.basicConfig.assert_called_once_with(level=m_logging.DEBUG)
+        if upvoted:
+            export_saved.save_upvoted.assert_called_once_with(reddit)
+        elif all:
+            export_saved.save_upvoted.assert_called_once_with(reddit)
+            export_saved.save_saved.assert_called_once_with(reddit)
+            export_saved.save_submissions.assert_called_once_with(reddit)
+            export_saved.save_comments.assert_called_once_with(reddit)
+        else:
+            export_saved.save_saved.assert_called_once_with(reddit)
